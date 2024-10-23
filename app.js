7034f7f06bc7691a6e0b48f9c52718e2f0cbf873
Ramon <johnnyramon2011@hotmail.com>
2024-10-23 16:23:45 -0300

    cors

--- app.js
+++ app.js
@@ -15,6 +15,20 @@ const corsOptions = {
   };
 var app = express();
 app.use(cors(corsOptions));
+
+// Middleware personalizado para restringir acesso localmente
+const allowedOrigins = ['https://traca.voxcity.com.br'];
+app.use((req, res, next) => {
+    const origin = req.headers.origin;
+    if (allowedOrigins.includes(origin) || !origin) {
+        // Permitir solicitações da origem permitida ou solicitações sem origem (acesso local)
+        next();
+    } else {
+        // Bloquear solicitações de outras origens
+        res.status(403).send('Access denied');
+    }
+});
+
 app.use(logger("dev"));
 app.use(cookieParser());
 app.use(bodyParser.json({ limit: "50mb" }));
