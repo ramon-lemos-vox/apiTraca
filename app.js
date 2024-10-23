7621f02506b846b6e95272fed148c223482911f3
Ramon <johnnyramon2011@hotmail.com>
2024-10-23 16:28:15 -0300

    cors

--- app.js
+++ app.js
@@ -7,28 +7,9 @@ var cors = require('cors');
 const HelperApp = require("./Models/Helper/HelperApp");
 
 var usersRouter = require('./routes/users');
-const corsOptions = {
-    origin: ['https://traca.voxcity.com.br'], // Permite apenas solicitações dessa origem
-    methods: ['GET', 'POST', 'PUT', 'DELETE'], // Métodos permitidos
-    optionsSuccessStatus: 200,
-    credentials: true
-  };
+
 var app = express();
-app.use(cors(corsOptions));
-
-// Middleware personalizado para restringir acesso localmente
-const allowedOrigins = ['https://traca.voxcity.com.br'];
-app.use((req, res, next) => {
-    const origin = req.headers.origin;
-    if (allowedOrigins.includes(origin) || !origin) {
-        // Permitir solicitações da origem permitida ou solicitações sem origem (acesso local)
-        next();
-    } else {
-        // Bloquear solicitações de outras origens
-        res.status(403).send('Access denied');
-    }
-});
-
+app.use(cors());
 app.use(logger("dev"));
 app.use(cookieParser());
 app.use(bodyParser.json({ limit: "50mb" }));
